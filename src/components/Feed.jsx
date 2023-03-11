5db4fb66f279002c8460937cd772a345cc398b3a
dontrellthedeveloper <dontrellknight@gmail.com>
2023-03-11 13:25:51 -0500

    added base feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
 
-export default Feed
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+       Sidebar
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2023 Dontrell Dev
+        </Typography>
+      </Box>
+
+
+    </Stack>
+  );
+};
+
+export default Feed;
