f47a964982d7503f97e686ee1161a1297b738f15
dontrellthedeveloper <dontrellknight@gmail.com>
2023-03-11 13:52:44 -0500

    added basic setup for sidebar component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar } from "./";
 
 
 
@@ -9,7 +10,7 @@ const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-       Sidebar
+       <Sidebar/>
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2023 Dontrell Dev
